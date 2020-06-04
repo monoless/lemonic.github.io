bb80baae030f991d3dc42d41a4974dc86b8ddd4a
LEMONIC <breakzingy@gmail.com>
2020-06-04 18:29:01 +0900

    init

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"https:/lemonic.github.io/2018/07/01/welcome.html"},{'title':"Welcome to Jekyll!",'url':"https:/lemonic.github.io/jekyll/update/2020/06/04/welcome-to-jekyll.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"Welcome to Jekyll!",'url':"/jekyll/update/2020/06/04/welcome-to-jekyll.html"}]};
